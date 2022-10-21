242f3545c65514fe6d851e2e4f8d91fd8cd9c062
Khôi <quangkhoi1228>
2022-10-22 01:00:07 +0700

    Update postgresql connector

--- src/main.py
+++ src/main.py
@@ -1,3 +1,39 @@
-print("hello")
+import posgresql.postgresql_connector as posgres_connector
 
+if __name__ == '__main__':
+    db_connection = posgres_connector.connect()
 
+    # create a cursor
+    cur = db_connection.cursor()
+        
+    query = "SELECT * FROM tbatoolnewstype ORDER BY id DESC"
+    # fetch all
+    cur.execute(query)
+    records = cur.fetchall()
+    print(records)
+
+    # fetch one 
+    cur.execute(query)
+    records = cur.fetchone()
+    print(records)
+
+    # fetch many 
+    cur.execute(query)
+    records = cur.fetchmany(3)
+    print(len(records))
+    print(records)
+
+    # fetch with param 
+    query = "SELECT * FROM tbatoolnewstype WHERE id = %s ORDER BY id DESC"
+    cur.execute(query,("2"))
+    records = cur.fetchall()
+    print(records)
+
+    # insert data
+    query = """
+    INSERT INTO tbatoolnewstype
+            (createddate, createduser, lastmodifieddate, lastmodifieduser, "type", childtype, childtypenameinvn, typenameinvn)
+            VALUES(%s, %s, %s, %s, %s, %s, %s, %s);
+            """
+    cur.execute(query,("2022-10-23", "test", "2022-10-23", "test", "TEST","INTERNAL_TRANSACTIONS","Giao dịch nội bộ","Thông tin công bố" ))
+    db_connection.commit()
--- src/posgresql/postgresql_connector.py
+++ src/posgresql/postgresql_connector.py
@@ -1,10 +1,28 @@
 # create connection syntax
 # conn = psycopg2.connect("dbname=suppliers user=postgres password=postgres")
 
+import psycopg2
+from posgresql.connection_config import *
 
+def connect():
+    """ Connect to the PostgreSQL database server """
+    db_connection = None
+    try:
+        db_connection = psycopg2.connect(
+            host=connection_host,
+            port=connection_port,
+            database=connection_database,
+            user=connection_user,
+            password=connection_password)
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="suppliers",
-    user="postgres",
-    password="Abcd1234")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if db_connection is not None:
+            print('Database connect successfully.')
+            return db_connection
+        else:
+            print("Database connect error")
+            exit()
+            
